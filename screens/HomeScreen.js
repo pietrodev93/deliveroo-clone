e9287f883eaa8c1fdae6308c00fbcd6f692af020
pietrodev93 <pietrogiordano93@gmail.com>
2022-07-12 23:55:20 +0200

    fetched data of featured restaurants

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,7 +6,7 @@ import {
   TextInput,
   ScrollView,
 } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
   AdjustmentsIcon,
@@ -16,68 +16,72 @@ import {
 } from "react-native-heroicons/outline";
 import Categories from "../components/Categories";
 import FeaturedRow from "../components/FeaturedRow";
+import sanityClient from "../sanity";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([])
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, []);
+  useEffect(() => {
+    sanityClient.fetch(`
+    *[_type == "featured"] {
+      ...,
+      restaurant[]->{
+        ...,
+        dishes[]->
+      }
+    }`).then(data => {
+      setFeaturedCategories(data)
+    })
+  },[])
   return (
     <SafeAreaView className="bg-white pt-5">
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-            <Image
-            source={{
-                uri: "https://links.papareact.com/wru",
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-            />
-            <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-            <Text className="font-bold text-xl">
-                Current Location
-                <ChevronDownIcon sie={20} color="#00CCBB" />
-            </Text>
-            </View>
-            <UserIcon size={35} color="#00CCBB" />
-        </View>
-        <View className="flex-row items-center space-x-2 pb-2 mx-4">
-            <View className="flex-row space-x-2 bg-gray-200 p-3 flex-1">
-            <SearchIcon color="gray" size={20} />
-            <TextInput
-                placeholder="Restaurants and cusines"
-                keyboardType="default"
-            />
-            </View>
-            <AdjustmentsIcon color="#00CCBB" />
-        </View>
-        <ScrollView
-            className="bg-gray-100"
-            contentContainerStyle={{
-            paddingBottom: 100,
-            }}
-        >
-            <Categories />
-            <FeaturedRow 
-                id='1'
-                title='Featured'
-                description='Paid placement from our partners'
-                featuredCategory='featured'
-            />
-            <FeaturedRow 
-                id='2'
-                title='Featured'
-                description='Paid placement from our partners'
-                featuredCategory='featured'
-            />
-            <FeaturedRow 
-                id='3'
-                title='Featured'
-                description='Paid placement from our partners'
-                featuredCategory='featured'
-            />
-        </ScrollView>
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+          source={{
+              uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon sie={20} color="#00CCBB" />
+          </Text>
+          </View>
+          <UserIcon size={35} color="#00CCBB" />
+      </View>
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+          <View className="flex-row space-x-2 bg-gray-200 p-3 flex-1">
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+              placeholder="Restaurants and cusines"
+              keyboardType="default"
+          />
+          </View>
+          <AdjustmentsIcon color="#00CCBB" />
+      </View>
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+        paddingBottom: 100,
+        }}
+      >
+        <Categories />
+        {featuredCategories?.map((category) => (
+          <FeaturedRow 
+            key={category._id}
+            id={category._id}
+            title={category.name}
+            description={category.short_description}
+          />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
