6805824e89204c6b732edcab8578569c073f01c2
pietrodev93 <pietrogiordano93@gmail.com>
2022-07-13 21:08:55 +0200

    Changed image width

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -7,7 +7,7 @@ const CategoryCard = ({imgUrl, title}) => {
         <Image source={{
             uri: imgUrl,
         }}
-        className='h-20 w-20 rounded'
+        className='h-20 w-24 rounded'
         />
       <Text className='absolute bottom-1 left-1 text-white font-bold'>{title}</Text>
     </TouchableOpacity>
